6f29c562c61cfad3002381e7f5925f976598b357
Luthfi Abdurrahim <luthfi@learn-quran.co>
2019-09-22 13:43:35 +0700

    Initial working fine Koran Dice

---
+++ Koran Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Koran Dice
+//
+//  Created by Luthfi Learn Quran on 21/09/19.
+//  Copyright © 2019 Luthviar Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
